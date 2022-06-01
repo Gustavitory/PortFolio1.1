5316765af2be27265371e314e43d4463dd0506e1
Gustavo Riera <gustavoriera@Gustavos-MacBook-Air.local>
2022-06-01 00:26:36 -0400

    bug fix movile

--- src/Components/Bowl.tsx
+++ src/Components/Bowl.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
 import { SectionCont } from '../Styles/Bowl/BowlStyles'
 import '../Styles/Bowl/BowlStyles.css'
+import { useState } from 'react';
+import React,{useEffect} from 'react';
 
 export const Bowl = () => {
+  const [calm,setCalm]=useState(true)
+  useEffect(()=>{
+    setTimeout(()=>setCalm(false),500)
+  })
   return (
+    calm?<h1>loading</h1>:
     <SectionCont>
         <div className='bowl'>
             <div className='liquid'/>
